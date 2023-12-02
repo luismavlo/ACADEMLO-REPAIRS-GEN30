83270a302d27656b4485383291d15d23ebe662a0
Luis Miguel <lmavendano1597@gmail.com>
2023-12-02 11:12:18 -0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { router as usersRoute } from '../modules/users/users.route.js';
+import { router as repairsRaute } from '../modules/repairs/repairs.route.js';
+
+export const router = express.Router();
+
+router.use('/repairs', repairsRaute)
+router.use('/users', usersRoute)
+
+
+
+
